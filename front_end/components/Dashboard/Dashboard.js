0b40c8fba1e6693954b3f2a651069958097f486c
ganguly2021 <sandeepganguly21@gmail.com>
2022-02-04 18:54:28 +0530

    dashboard session redirect changes.

--- front_end/components/Dashboard/Dashboard.js
+++ front_end/components/Dashboard/Dashboard.js
@@ -7,12 +7,18 @@ import {
   showFooter,
 } from "./../../redux/reducers/visible";
 import { useDispatch } from "react-redux";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useSnackbar } from "notistack";
 
 function Dashboard({ children }) {
   const [isMounted, setMounted] = useState(false);
+  const { data: session, status } = useSession();
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
 
   const dispatch = useDispatch();
-  
+  const router = useRouter();
+
   useEffect(() => {
     // set mounted to true
     setMounted(true);
@@ -32,7 +38,20 @@ function Dashboard({ children }) {
 
       dispatch(showFooter());
     };
-  });
+  }, []);
+
+  // if user not logged in
+  if (status !== "authenticated") {
+    // show error message
+    enqueueSnackbar("Only logged in user can access dashboard.", {
+      variant: "error",
+      autoHideDuration: 1500,
+    });
+
+    // redirect user to login page
+    router.push("/login");
+  }
+
   return <DashboardView rightContent={children} />;
 }
 
